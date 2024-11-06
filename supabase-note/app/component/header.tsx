991798951ac797c00f36b03afdfaecdaed66fd11
wilderif <woojuro3@gmail.com>
2024-11-07 01:22:38 +0900

    feat: Implement UI component and Header component

---
+++ supabase-note/app/component/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-green-700 px-4 py-3">
+      <img src="/supanote-logo.png" alt="Supanote Logo" className="h-7 " />
+    </header>
+  );
+}
---
+++ supabase-note/app/ui.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Header from "./component/header";
+
+export default function UI() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
