c9525c7fda686d4faaa59aad99cb9ea2c370fa86
wilderif <woojuro3@gmail.com>
2025-01-15 11:36:37 +0900

    chore(todo-list): Add prettier config file  for tailwind plugins

--- supabase-todo-list/src/app/page.tsx
+++ supabase-todo-list/src/app/page.tsx
@@ -1,8 +1,9 @@
+import UI from './ui';
+
 export default function Home() {
   return (
     <main>
-      TODO List
-      <i className="fas fa-check"></i>
+      <UI />
     </main>
   );
 }
---
+++ supabase-todo-list/src/app/ui.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Input } from '@material-tailwind/react';
+
+export default function UI() {
+  return (
+    <div className="mx-auto flex w-2/3 flex-col items-center gap-2 py-10">
+      <h1 className="text-xl">TODO LIST</h1>
+      <Input
+        label="Search TODO"
+        placeholder="Search TODO"
+        icon={<i className="fas fa-search"></i>}
+        onPointerEnterCapture={() => {}}
+        onPointerLeaveCapture={() => {}}
+        crossOrigin={undefined}
+      />
+    </div>
+  );
+}
