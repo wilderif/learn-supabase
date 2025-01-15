a10a9c52516e355ee64da77ebfdd4ebdf7970e3b
wilderif <woojuro3@gmail.com>
2025-01-15 12:05:52 +0900

    feat(todo-list): Add UI elements
    - Added a search bar, todo list, buttons for add, edit, delete and complete

--- supabase-todo-list/src/app/ui.tsx
+++ supabase-todo-list/src/app/ui.tsx
@@ -1,19 +1,23 @@
 'use client';
-
-import { Input } from '@material-tailwind/react';
+import { Button, Input } from '@material-tailwind/react';
+import Todo from '@/components/todo';
 
 export default function UI() {
   return (
     <div className="mx-auto flex w-2/3 flex-col items-center gap-2 py-10">
       <h1 className="text-xl">TODO LIST</h1>
+
       <Input
         label="Search TODO"
         placeholder="Search TODO"
-        icon={<i className="fas fa-search"></i>}
-        onPointerEnterCapture={() => {}}
-        onPointerLeaveCapture={() => {}}
-        crossOrigin={undefined}
+        icon={<i className="fas fa-search" />}
       />
+
+      <Todo />
+      <Button>
+        <i className="fas fa-plus mr-2" />
+        ADD TODO
+      </Button>
     </div>
   );
 }
