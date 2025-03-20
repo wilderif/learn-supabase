8fe826544f699077e013e03fdb5bb4a996dcd851
wilderif <woojuro3@gmail.com>
2025-03-20 19:40:21 +0900

    feat(netflix-clone): Add SEO metadata using generateMetadata for movie details
    - Implement generateMetadata to dynamically set page title and description
    - Add Open Graph metadata for better sharing and preview support

--- supabase-netflix-clone/src/app/movies/[id]/page.tsx
+++ supabase-netflix-clone/src/app/movies/[id]/page.tsx
@@ -1,6 +1,25 @@
 import { getMovie } from '@/actions/movie-actions';
 import MoviesUI from './ui';
 
+export async function generateMetadata({
+  params,
+  // searchParams,
+}: {
+  params: { id: string };
+  // searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const id = Number(params.id);
+  const movie = await getMovie(id);
+
+  return {
+    title: movie?.title,
+    description: movie?.overview,
+    openGraph: {
+      images: [movie?.image_url],
+    },
+  };
+}
+
 export default async function Movies({ params }: { params: { id: string } }) {
   const movie = await getMovie(Number(params.id));
 
